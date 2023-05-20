641293c8f6a67376d4bf3f5809b4e233d6487b14
Kacper Prawda <skipzyeah@gmail.com>
2023-05-20 12:29:09 +0200

    FIX: netlify, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, series } = require("gulp");
+const { src, dest, watch, series, gulp, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
@@ -6,6 +6,11 @@ const cssnano = require("cssnano");
 const babel = require("gulp-babel");
 const terser = require("gulp-terser");
 const browsersync = require("browser-sync").create();
+
+const connect = require("gulp-connect"); // Runs a local webserver
+const open = require("gulp-open"); // Opens a URL in a web browser
+const exec = require("child_process").exec; // run command-line programs from gulp
+const execSync = require("child_process").execSync; // command-line reports
 
 //sass task
 function scssTask() {
@@ -23,34 +28,78 @@ function jsTask() {
 }
 
 //reloading website
-function browserSyncServe(cb) {
-  browsersync.init({
-    server: {
-      baseDir: ".",
-    },
-    notify: {
-      styles: {
-        top: "auto",
-        bottom: "0",
-      },
-    },
-    browser: "google chrome",
-  });
-  cb();
-}
-function browserSyncReload(cb) {
-  browsersync.reload();
-  cb();
-}
+// function browserSyncServe(cb) {
+//   browsersync.init({
+//     server: {
+//       baseDir: ".",
+//     },
+//     notify: {
+//       styles: {
+//         top: "auto",
+//         bottom: "0",
+//       },
+//     },
+//     browser: "google chrome",
+//   });
+//   cb();
+// }
+// function browserSyncReload(cb) {
+//   browsersync.reload();
+//   cb();
+// }
 
 //watch task -- if any changes then do reload
-function watchTask() {
-  watch("*.html", browserSyncReload);
-  watch(
-    ["app/scss/**/*.scss", "app/**/*.js"],
-    series(scssTask, jsTask, browserSyncReload)
-  );
+// function watchTask() {
+//   watch("*.html", browserSyncReload);
+//   watch(
+//     ["app/scss/**/*.scss", "app/**/*.js"],
+//     series(scssTask, jsTask, browserSyncReload)
+//   );
+// }
+function openBrowser(done) {
+  var options = {
+    uri: "http://localhost:8080",
+  };
+  return src("./").pipe(open(options));
+  done();
 }
 
+// Gulp plugin to run a webserver (with LiveReload)
+// https://www.npmjs.com/package/gulp-connect
+function server(done) {
+  return connect.server({
+    root: "./",
+    port: 8080,
+    debug: true,
+  });
+  done();
+}
+
+// Commit and push files to Git
+function git(done) {
+  return exec('git add . && git commit -m "netlify deploy" && git push');
+  done();
+}
+
+// Watch for netlify deployment
+function netlify(done) {
+  return new Promise(function (resolve, reject) {
+    console.log(execSync("netlify watch").toString());
+    resolve();
+  });
+}
+
+// Preview Deployment
+function netlifyOpen(done) {
+  return exec("netlify open:site");
+  done();
+}
+
+// Deploy command
+exports.deploy = series(git, netlify, netlifyOpen);
+
+// Default Gulp command
+exports.default = series(scssTask, jsTask, openBrowser, server);
+
 //default gulp task
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+//exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
